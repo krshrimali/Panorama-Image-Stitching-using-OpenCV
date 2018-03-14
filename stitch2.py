66e6dd8d21ef30c17ebc42bfc487b03927d78fb4
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2018-03-14 14:31:42 +0530

    Added opencv version check

--- stitch2.py
+++ stitch2.py
@@ -7,6 +7,8 @@ image_right = cv2.imread(input("Second image: "), 1)
 cv2.resize(image_left, image_left, Size(), 0.5, 0.5, interpolation)
 cv2.resize(image_right, image_right, Size(), 0.5, 0.5, interpolation)
 
+# checking commit 
+cv2.is_v3() # checks if opencv version is 3.3+ or not
 
 cv2.imshow("image_left", image_left)
 cv2.imshow("image_right", image_right)
