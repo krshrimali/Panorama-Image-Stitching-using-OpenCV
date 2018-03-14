fbe1104ce28243b93bb5937ae6604c5826b5b964
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2018-03-14 07:31:19 +0000

    Added initial files

---
+++ stitch2.py
@@ -0,0 +1,15 @@
+import cv2
+
+image_left = cv2.imread(input("First image: "), 1)
+image_right = cv2.imread(input("Second image: "), 1)
+
+# resize the images
+cv2.resize(image_left, image_left, Size(), 0.5, 0.5, interpolation)
+cv2.resize(image_right, image_right, Size(), 0.5, 0.5, interpolation)
+
+
+cv2.imshow("image_left", image_left)
+cv2.imshow("image_right", image_right)
+
+cv2.waitKey(0)
+cv2.DestroyAllWindows()
